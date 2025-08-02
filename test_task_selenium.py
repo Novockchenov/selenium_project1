3ce02ea91e2f2327a0e9c6976e857c8be36bffdf
Sergey <generals1986@yandex.ru>
2025-08-03 00:27:29 +0300

    Добавил изменения для теста Steam

--- test_task_selenium.py
+++ test_task_selenium.py
@@ -1,65 +1,81 @@
 import pytest
-import time
+from faker import Faker
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+STEAM_URL = "https://store.steampowered.com/"
+WAIT_TIME = 15
+
+# Чтобы убедиться, что страница загрузилась
+STEAM_LOGO_LOCATOR = (By.XPATH, "//div[@id='global_header']//a[contains(@href, 'steampowered.com')]")
+
+LOGIN_BUTTON_LOCATOR = (By.XPATH, "//a[@class='global_action_link' and text()='войти']")
+
+USERNAME_INPUT_LOCATOR = (
+    By.XPATH, "//form//input[@type='text']")
+
+PASSWORD_INPUT_LOCATOR = (
+    By.XPATH, "//form//input[@type='password']")
+
+SIGN_IN_BUTTON_LOCATOR = (By.XPATH, "//form//button[@type='submit']")
+
+LOADING_SPINNER_LOCATOR = (By.XPATH, "//form//button[contains(@class, ' ') and @type='submit']")
+
+ERROR_LOCATOR = (By.XPATH, "//form//div[contains(text(), 'Пожалуйста, проверьте свой пароль')]")
+
+# //form//button[@type='submit']//following-sibling::div[1]
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome()
     driver.maximize_window()
     yield driver
     driver.quit()
 
 
 def test_steam(driver):
-    driver.get("https://store.steampowered.com/")
+    fake = Faker()
 
-    # ждём 15 секунд
-    wait = WebDriverWait(driver, 15)
+    username_data = fake.user_name()
 
-    login_button = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "global_action_link")))
+    password_data = fake.password(length=16, special_chars=True, upper_case=True, lower_case=True, digits=True)
 
-    assert login_button.is_displayed()  # , "Кнопка Войти не загрузилась"
+    driver.get(STEAM_URL)
+
+    wait = WebDriverWait(driver, WAIT_TIME)
+
+    # Убеждаемся в загрузке страницы.
+    wait.until(EC.visibility_of_element_located(STEAM_LOGO_LOCATOR))
+
+    login_button = wait.until(EC.element_to_be_clickable(LOGIN_BUTTON_LOCATOR))
 
     login_button.click()
 
-    username = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[type = "text"]')))
+    # Заполняем поля
+    username = wait.until(EC.visibility_of_element_located(USERNAME_INPUT_LOCATOR))
+    password = wait.until(EC.visibility_of_element_located(PASSWORD_INPUT_LOCATOR))
 
-    username.is_displayed()
+    username.send_keys(username_data)
+    password.send_keys(password_data)
 
-    password = driver.find_element(By.CSS_SELECTOR, 'input[type="password"]')
+    sign_in_button = wait.until(EC.element_to_be_clickable(SIGN_IN_BUTTON_LOCATOR))
 
-    button_signin = wait.until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit'][class='DjSvCZoKKfoNSmarsEcTS']"))
-    )
+    sign_in_button.click()
 
-    username.send_keys("Иванов")
-    password.send_keys("123retef")
+    # Загрузка и исчезновение спиннера
+    wait.until(EC.presence_of_element_located(LOADING_SPINNER_LOCATOR))
+    wait.until(EC.invisibility_of_element_located(LOADING_SPINNER_LOCATOR))
 
-    button_signin.click()
+    # Текст ошибки
+    error_message = wait.until(EC.visibility_of_element_located(ERROR_LOCATOR))
 
-    # time.sleep(4)
+    expected_error = "Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова."
+    actual_error = error_message.text.strip()
 
-    # button_signin2 = wait.until(
-    #   EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]'))
-    # )
-
-    # class="DjSvCZoKKfoNSmarsEcTS _2NVQcOnbtdGIu9O-mB9-YE"
-    loading_button_locator = (By.CSS_SELECTOR, "button[type='submit'][class='_2NVQcOnbtdGIu9O-mB9-YE']")
-
-    # появление второго класса (спиннера)
-    wait.until(EC.presence_of_element_located(loading_button_locator))
-
-    # второй класс исчезает и исчезает спиннер
-    wait.until(EC.invisibility_of_element_located(loading_button_locator))
-
-    error_locator = (By.XPATH, "//div[contains(@class, '_1W_6HXiG4JJ0By1qN_0fGZ')]")
-    error_message = wait.until(EC.visibility_of_element_located(error_locator))
-
-    assert "Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова." in error_message.text
+    assert expected_error in actual_error, \
+        f"Текст ошибки не соответствует ожидаемому.\n" \
+        f"Ожидаемый текст содержит: '{expected_error}'\n" \
+        f"Фактический текст: '{actual_error}'"
